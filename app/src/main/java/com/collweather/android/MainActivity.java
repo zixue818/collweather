acf970a693bf2c36a417eafedf87b2fd23978889
ljc <2812338@qq.com>
2018-10-30 15:07:23 +0800

    commit at school 2018-10-30 15:07

--- app/src/main/java/com/collweather/android/MainActivity.java
+++ app/src/main/java/com/collweather/android/MainActivity.java
@@ -3,6 +3,8 @@ package com.collweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//这是一些注释，用来检测同步是否成功
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
